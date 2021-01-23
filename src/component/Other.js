4e7418b61e4e50efa5630537f30980b1575c1ea7
Playneko <playneko@naver.com>
2021-01-23 10:30:02 +0900

    React Firebase
    기타메뉴 수정

--- src/component/Other.js
+++ src/component/Other.js
@@ -1,4 +1,8 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import List from '@material-ui/core/List';
+import Avatar from '@material-ui/core/Avatar';
+import ListItemAvatar from '@material-ui/core/ListItemAvatar';
 
 // 컴포넌트
 // 로그인 체크
@@ -10,13 +14,33 @@ import Footer from "./Footer";
 // CSS
 import '../styles/App.css';
 
+const useStyles = makeStyles((theme) => ({
+  root: {
+    width: '100%',
+  },
+}));
+
 const Other = (props) => {
+  const classes = useStyles();
+  const auth = props.children;
+
   // 로그인 체크
   CheckLogin(props);
 
   return (
     <>
       <Header />
+      <List dense className={classes.root + " list-top"}>
+        <ListItemAvatar className="other-avatar">
+          <Avatar
+            alt={auth.name}
+            src={auth.image}
+          />
+        </ListItemAvatar>
+        <div className="other-name">
+          <p>{auth.name}</p>
+        </div>
+      </List>
       <Footer />
     </>
   );
